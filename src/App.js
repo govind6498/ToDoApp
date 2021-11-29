b71523e8f68870fa80a1d9e202d439f46dd47baf
govind6498 <govindsingh6498@gail.com>
2021-11-29 12:38:24 +0530

    Added:Explore Todo App code

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./App.css";
+import ToDoLists from "./TodoList";
+
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+  const listOfItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+  const deleteItems = (id) => {
+    console.log("deleted" + id);
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+  return (
+    <>
+      <div className="main-div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a item"
+            value={inputList}
+            onChange={itemEvent} />
+          <button onClick={listOfItem}><strong>+</strong></button>
+          <ol>
+            {Items.map((itemval, index) => {
+              return <ToDoLists
+                key={index}
+                id={index}
+                text={itemval}
+                onSelect={deleteItems}
+              />
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
